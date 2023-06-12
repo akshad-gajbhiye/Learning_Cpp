e38a8a4b8019f336cf008e492addb3d24fe779c4
Akshad <over1over2over@gmail.com>
2023-06-12 14:25:16 +0530

    1st program

---
+++ 1_hello.cc
@@ -0,0 +1,20 @@
+/*
+Original Author(s): Akshad Vivek Gajbhiye
+Date: 12th June 2023   
+
+This program is full of rizz and if u share this github repo with a nerd girl, she will fall for you.
+*/
+#include<stdio.h>
+
+/* 
+This program displays ultimate rizz
+Input: None
+Output: None
+*/
+int main()
+{
+    //Ultimate rizz
+    printf("Hello babes\n");
+    printf("\tHow you doin??\n");
+    printf("I m ready to get railed\n");
+}
